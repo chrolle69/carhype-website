322e578f907bbb0329f015b5acad198385a8eb15
lucas <lgul@itu.dk>
2025-08-29 00:28:37 +0200

    removed unsused

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function AnalyticsPage() {
     const [data, setData] = useState<any>(null);
