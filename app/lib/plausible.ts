676b36b9b56efdd1d496bff4dd72c66e95139a2e
lucas <lgul@itu.dk>
2025-08-29 16:11:08 +0200

    linting erros

--- app/lib/plausible.ts
+++ app/lib/plausible.ts
@@ -1,15 +1,19 @@
-"use client"
-
-export { };
+"use client";
 
 declare global {
     interface Window {
-        plausible?: (event: string, options?: { props?: Record<string, unknown> }) => void;
+        plausible?: (
+            event: string,
+            options?: { props?: Record<string, unknown> }
+        ) => void;
     }
 }
 
-export function trackPlausible(event: string, props: Record<string, any> = {}) {
+export function trackPlausible(
+    event: string,
+    props: Record<string, unknown> = {}
+) {
     if (typeof window !== "undefined" && typeof window.plausible === "function") {
-        window.plausible(event, { props })
+        window.plausible(event, { props });
     }
 }
